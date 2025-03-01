858563d590b9ba455624e83fde5d8a515d089902
bKatiukha <bohdankatuha@gmail.com>
2025-03-01 19:54:24 +0200

    remove duplication in Minesweeper class

--- minesweeper.py
+++ minesweeper.py
@@ -58,7 +58,7 @@ class Minesweeper:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < self.board_size and 0 <= ny < self.board_size:
+                    if self._is_valid_coordinate(nx, ny):
                         self.reveal(nx, ny)
 
     def flag(self, x, y):
